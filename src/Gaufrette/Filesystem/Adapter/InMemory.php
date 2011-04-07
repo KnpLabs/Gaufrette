2b76044f41bf72b8aa24a2dee03ab1c45ffe888a
Antoine Hérault <antoine.herault@gmail.com>
2011-04-07 10:58:14 +0200

    Add in memory adapter for test purposes

---
+++ src/Gaufrette/Filesystem/Adapter/InMemory.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Gaufrette\Filesystem\Adapter;
+
+use Gaufrette\Filesystem\Adapter;
+use Gaufrette\Checksum;
+
+/**
+ * In memory adapter
+ *
+ * Stores some files in memory for test purposes
+ *
+ * @package Gaufrette
+ * @author Antoine Hérault <antoine.herault@gmail.com>
+ */
+class InMemory implements Adapter
+{
+    protected $files = array();
+
+    /**
+     * Constructor
+     *
+     * @param  array $files An array of files
+     */
+    public function __construct(array $files = array())
+    {
+        $this->setFiles($files);
+    }
+
+    /**
+     * Defines the files
+     *
+     * @param  array $files An array of files
+     */
+    public function setFiles(array $files)
+    {
+        $this->files = array();
+        foreach ($files as $key => $file)
+        {
+            if (!is_array($file)) {
+                $file = array('content' => $file);
+            }
+
+            $file = array_merge(array(
+                'content'   => null,
+                'mtime'     => null,
+                'checksum'  => null
+            ), $file);
+
+            $this->setFile($key, $file['content'], $file['mtime'], $file['checksum']);
+        }
+    }
+
+    /**
+     * Defines a file
+     *
+     * @param  string  $key      The key
+     * @param  string  $content  The content
+     * @param  integer $mtime    The last modified time (automatically set to now if NULL)
+     * @param  string  $checksum The checksum (automatically computed from the content if NULL)
+     */
+    public function setFile($key, $content = null, $mtime = null, $checksum = null)
+    {
+        if (null === $mtime) {
+            $mtime = time();
+        }
+
+        if (null === $checksum) {
+            $checksum = Checksum::fromContent($content);
+        }
+
+        $this->files[$key] = array(
+            'content'   => (string)  $content,
+            'mtime'     => (integer) $mtime,
+            'checksum'  => (string)  $checksum
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function read($key)
+    {
+        return $this->files[$key]['content'];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function write($key, $content)
+    {
+        $this->files[$key]['content']  = $content;
+        $this->files[$key]['mtime']    = time();
+        $this->files[$key]['checksum'] = Checksum::fromContent($content);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function exists($key)
+    {
+        return array_key_exists($key, $this->files);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function keys($pattern)
+    {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function mtime($key)
+    {
+        return $this->files[$key]['mtime'];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function checksum($key)
+    {
+        return $this->files[$key]['checksum'];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function delete($key)
+    {
+        unset($this->files[$key]);
+
+        return true;
+    }
+}
---
+++ tests/Filesystem/Adapter/InMemoryTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Gaufrette\Filesystem\Adapter;
+
+use Gaufrette\Checksum;
+
+class InMemoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSetFiles()
+    {
+        $adapter = new InMemory(array(
+            'foo' => 'Foo content',
+            'bar' => 'Bar content'
+        ));
+
+        $this->assertFalse($adapter->exists('foobar'));
+        $this->assertTrue($adapter->exists('foo'));
+        $this->assertEquals('Foo content', $adapter->read('foo'));
+        $this->assertTrue($adapter->exists('bar'));
+        $this->assertEquals('Bar content', $adapter->read('bar'));
+    }
+
+    public function testWriteAndRead()
+    {
+        $adapter = new InMemory();
+
+        $this->assertFalse($adapter->exists('foobar'));
+
+        $adapter->write('foobar', 'Some content');
+
+        $this->assertTrue($adapter->exists('foobar'));
+        $this->assertEquals('Some content', $adapter->read('foobar'));
+    }
+
+    public function testChecksumIsAutomaticallySetFromContent()
+    {
+        $adapter = new InMemory(array(
+            'foobar' => 'Some content'
+        ));
+
+        $this->assertEquals(Checksum::fromContent('Some content'), $adapter->checksum('foobar'));
+    }
+
+    public function testManuallyDefineChecksum()
+    {
+        $adapter = new InMemory(array(
+            'foobar' => array(
+                'content'  => 'Some content',
+                'checksum' => 'abcd'
+            )
+        ));
+
+        $this->assertEquals('abcd', $adapter->checksum('foobar'));
+    }
+
+    public function testChecksumIsUpdatedOnWrite()
+    {
+        $adapter = new InMemory(array(
+            'foobar' => array(
+                'content'  => 'Some content',
+                'checksum' => 'abcd'
+            )
+        ));
+
+        $adapter->write('foobar', 'Changed content');
+
+        $this->assertEquals(Checksum::fromContent('Changed content'), $adapter->checksum('foobar'));
+    }
+
+    public function testLastModifiedTimeIsAutomaticallySet()
+    {
+        $time = time();
+        $adapter = new InMemory(array(
+            'foobar' => 'Some content'
+        ));
+
+        $this->assertTrue(in_array($adapter->mtime('foobar'), array($time, $time + 1)));
+    }
+
+    public function testManuallyDefineLastModifiedTime()
+    {
+        $adapter = new InMemory(array(
+            'foobar' => array(
+                'content' => 'Some content',
+                'mtime'   => 123456789
+            )
+        ));
+
+        $this->assertEquals(123456789, $adapter->mtime('foobar'));
+    }
+
+    public function testLastModifiedTimeIsUpdatedOnWrite()
+    {
+        $adapter = new InMemory(array(
+            'foobar' => array(
+                'content' => 'Some content',
+                'mtime'   => 123456789
+            )
+        ));
+
+        $time = time();
+        $adapter->write('foobar', 'Changed content');
+
+        $this->assertTrue(in_array($adapter->mtime('foobar'), array($time, $time + 1)));
+    }
+}
