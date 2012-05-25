0d11841262208edc983aa501e11e624d75486cb4
Antoine Hérault <antoine.herault@gmail.com>
2012-05-25 17:22:36 +0200

    Use base functional test case for DBAL adapter

--- bin/install_vendors.sh
+++ bin/install_vendors.sh
@@ -3,3 +3,5 @@
 git clone https://github.com/symfony/ClassLoader.git vendor/symfony-class-loader
 git clone https://github.com/amazonwebservices/aws-sdk-for-php.git vendor/aws-sdk
 git clone https://github.com/Herzult/php-ssh.git vendor/php-ssh
+git clone https://github.com/doctrine/dbal.git vendor/doctrine-dbal
+git clone https://github.com/doctrine/common.git vendor/doctrine-common
