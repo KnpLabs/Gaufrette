da856d8327581300a2727f4d50885242bdb53990
Daniel Richter <nexyz9@gmail.com>
2012-05-30 20:54:12 -0700

    RS adapter: renaming fromString to fromContent

--- src/Gaufrette/Adapter/RackspaceCloudfiles.php
+++ src/Gaufrette/Adapter/RackspaceCloudfiles.php
@@ -66,7 +66,7 @@ class RackspaceCloudfiles extends Base
             throw new \RuntimeException(sprintf('Could not write the \'%s\' file.', $key));
         }
 
-        return Util\Size::fromString($content);
+        return Util\Size::fromContent($content);
     }
 
     /**
