53297bd0e36eca4e24e29de04c3bf5f417daba5c
l3l0 <leszek.prabucki@gmail.com>
2013-04-20 09:55:40 +0200

    Remove sudo from pecl command runned at travis.

--- bin/configure_test_env.sh
+++ bin/configure_test_env.sh
@@ -2,8 +2,8 @@
 
 sudo apt-get update -qq
 sudo apt-get install -qq libssh2-1-dev libssh2-php
-sudo pecl install mongo &> /dev/null
+pecl install mongo &> /dev/null
 touch .interactive
-(sudo pecl install -f ssh2-beta < .interactive)
+(pecl install -f ssh2-beta < .interactive)
 
 cp tests/Gaufrette/Functional/adapters/DoctrineDbal.php.dist tests/Gaufrette/Functional/adapters/DoctrineDbal.php -f
