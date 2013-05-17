14c68837c9712c2e21d98de51d0a5b261e788993
Zeichen32 <j.averkamp@itm-systems.com>
2013-05-17 22:37:21 +0200

    Fixed typo

--- src/Gaufrette/Adapter/Ftp.php
+++ src/Gaufrette/Adapter/Ftp.php
@@ -34,7 +34,7 @@ class Ftp implements Adapter,
      *
      * @param string $directory The directory to use in the ftp server
      * @param string $host      The host of the ftp server
-     * @param array  $options   The options like port, username, password, passive, create, mode, timeout_check
+     * @param array  $options   The options like port, username, password, passive, create, mode, check_timeout
      */
     public function __construct($directory, $host, $options = array())
     {
