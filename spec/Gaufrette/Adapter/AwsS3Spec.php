ff6da5461cfd2031c82c85519a6d5643cb97968e
Michael Dowling <mtdowling@gmail.com>
2013-08-07 16:03:04 -0700

    More spec tests for the AwsS3 adapter

--- spec/Gaufrette/Adapter/AwsS3Spec.php
+++ spec/Gaufrette/Adapter/AwsS3Spec.php
@@ -1,14 +1,32 @@
 <?php
 
-namespace spec\src\Gaufrette\Adapter;
+namespace spec\Gaufrette\Adapter;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class AwsS3Spec extends ObjectBehavior
 {
+    /**
+     * @param \Aws\S3\S3Client $service
+     */
+    function let($service)
+    {
+        $this->beConstructedWith($service, 'bucketName');
+    }
+
     function it_is_initializable()
     {
-        $this->shouldHaveType('src\Gaufrette\Adapter\AwsS3.php');
+        $this->shouldHaveType('Gaufrette\Adapter\AwsS3');
+    }
+
+    function it_is_adapter()
+    {
+        $this->shouldHaveType('Gaufrette\Adapter');
+    }
+
+    function it_supports_metadata()
+    {
+        $this->shouldHaveType('Gaufrette\Adapter\MetadataSupporter');
     }
 }
