53ec08c324ea32380c5782c5f2e4f8805d644113
Enrico Stahn <mail@enricostahn.com>
2014-02-19 11:28:20 +1100

    Restore error handler after test steps are finished

--- spec/Gaufrette/Stream/LocalSpec.php
+++ spec/Gaufrette/Stream/LocalSpec.php
@@ -16,12 +16,14 @@ class LocalSpec extends ObjectBehavior
 
     function it_throws_runtime_exception_when_file_doesnt_exists_and_custom_error_handler_specified()
     {
-        $custom_error_handler = function ($errno, $errstr, $errfile, $errline ) {
+        $custom_error_handler = function ($errno, $errstr, $errfile, $errline) {
             throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
         };
         set_error_handler($custom_error_handler);
 
         $this->beConstructedWith(vfsStream::url('other'));
         $this->shouldThrow('\RuntimeException')->duringOpen(new StreamMode('r'));
+
+        restore_error_handler();
     }
 }
