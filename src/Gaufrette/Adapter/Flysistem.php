260009f4299b4b5c54cd65a8f43d4078975da497
umpirsky <umpirsky@gmail.com>
2016-03-04 16:43:23 +0100

    Flysistem adapter

---
+++ src/Gaufrette/Adapter/Flysistem.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Gaufrette\Adapter;
+
+use Gaufrette\Adapter;
+use League\Flysystem\AdapterInterface;
+use League\Flysystem\Util;
+
+class Flysistem implements Adapter
+{
+    private $adapter;
+    private $config;
+
+    /**
+     * @param AdapterInterface  $adapter
+     * @param Config|array|null $config
+     */
+    public function __construct(AdapterInterface $adapter, $config = null)
+    {
+        $this->adapter = $adapter;
+        $this->config = Util::ensureConfig($config);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read($key)
+    {
+        return $this->adapter->read($key);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write($key, $content)
+    {
+        return $this->adapter->write($key, $content, $this->config);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function exists($key)
+    {
+        return $this->adapter->has($key);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function keys()
+    {
+        return $this->adapter->listContents();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function mtime($key)
+    {
+        return $this->adapter->getTimestamp($key);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($key)
+    {
+        return $this->adapter->delete($key);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rename($sourceKey, $targetKey)
+    {
+        return $this->adapter->rename($sourceKey, $targetKey);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isDirectory($key)
+    {
+        // TODO: create proper exception class if Flysistem does not support isDirectory()
+        throw new \BadMethodCallException('isDirectory is not supported by this adapter.');
+    }
+}
