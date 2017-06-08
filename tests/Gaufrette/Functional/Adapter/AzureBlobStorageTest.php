c966d14bc3f017866c063e94d896fe082f5c4423
Albin Kerouanton <albin.kerouanton@knplabs.com>
2017-06-08 11:38:49 +0200

    Rename environment variables related to Azure to follow its semantic

--- tests/Gaufrette/Functional/Adapter/AzureBlobStorageTest.php
+++ tests/Gaufrette/Functional/Adapter/AzureBlobStorageTest.php
@@ -14,14 +14,15 @@ class AzureBlobStorageTest extends FunctionalTestCase
 {
     public function setUp()
     {
+        $account = getenv('AZURE_ACCOUNT');
         $key = getenv('AZURE_KEY');
-        $secret = getenv('AZURE_SECRET');
         $containerName = getenv('AZURE_CONTAINER');
-        if (empty($key) || empty($secret) || empty($containerName)) {
-            $this->markTestSkipped('Either AZURE_KEY, AZURE_SECRET and/or AZURE_CONTAINER env variables are not defined.');
+
+        if (empty($account) || empty($key) || empty($containerName)) {
+            $this->markTestSkipped('Either AZURE_ACCOUNT, AZURE_KEY and/or AZURE_CONTAINER env variables are not defined.');
         }
 
-        $connection = sprintf('BlobEndpoint=http://%1$s.blob.core.windows.net/;AccountName=%1$s;AccountKey=%2$s', $key, $secret);
+        $connection = sprintf('BlobEndpoint=http://%1$s.blob.core.windows.net/;AccountName=%1$s;AccountKey=%2$s', $account, $key);
 
         $this->filesystem = new Filesystem(new AzureBlobStorage(new BlobProxyFactory($connection), $containerName, true));
     }
