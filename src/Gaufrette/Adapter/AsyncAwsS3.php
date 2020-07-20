c6cef07358ce7b0230eb1f91cc9b76de82ade82c
Nyholm <tobias.nyholm@gmail.com>
2020-07-20 16:07:55 +0200

    Adding support for AsyncAws

---
+++ src/Gaufrette/Adapter/AsyncAwsS3.php
@@ -0,0 +1,380 @@
+<?php
+
+namespace Gaufrette\Adapter;
+
+use AsyncAws\S3\Input\GetObjectRequest;
+use AsyncAws\SimpleS3\SimpleS3Client;
+use Gaufrette\Adapter;
+use Gaufrette\Util;
+
+/**
+ * Amazon S3 adapter using the AsyncAws.
+ *
+ * @author  Michael Dowling <mtdowling@gmail.com>
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+class AsyncAwsS3 implements Adapter, MetadataSupporter, ListKeysAware, SizeCalculator, MimeTypeProvider
+{
+    /** @var SimpleS3Client */
+    protected $service;
+    /** @var string */
+    protected $bucket;
+    /** @var array */
+    protected $options;
+    /** @var bool */
+    protected $bucketExists;
+    /** @var array */
+    protected $metadata = [];
+    /** @var bool */
+    protected $detectContentType;
+
+    /**
+     * @param SimpleS3Client $service
+     * @param string   $bucket
+     * @param array    $options
+     * @param bool     $detectContentType
+     */
+    public function __construct(SimpleS3Client $service, $bucket, array $options = [], $detectContentType = false)
+    {
+        $this->service = $service;
+        $this->bucket = $bucket;
+        $this->options = array_replace(
+            [
+                'create' => false,
+                'directory' => '',
+                'acl' => 'private',
+            ],
+            $options
+        );
+
+        $this->detectContentType = $detectContentType;
+    }
+
+    /**
+     * Gets the publicly accessible URL of an Amazon S3 object.
+     *
+     * @param string $key     Object key
+     * @param array  $options Associative array of options used to buld the URL
+     *                        - expires: The time at which the URL should expire
+     *                        represented as a UNIX timestamp
+     *                        - Any options available in the Amazon S3 GetObject
+     *                        operation may be specified.
+     *
+     * @return string
+     *
+     * @deprecated 1.0 Resolving object path into URLs is out of the scope of this repository since v0.4. gaufrette/extras
+     *                 provides a Filesystem decorator with a regular resolve() method. You should use it instead.
+     *
+     * @see https://github.com/Gaufrette/extras
+     */
+    public function getUrl($key, array $options = [])
+    {
+        @trigger_error(
+            'Using AwsS3::getUrl() method was deprecated since v0.4. Please check gaufrette/extras package if you want this feature',
+            E_USER_DEPRECATED
+        );
+
+        $path = $this->computePath($key);
+
+        if (isset($options['expires'])) {
+            $date = new \DateTimeImmutable('@'.$options['expires']);
+            $input = GetObjectRequest::create(array_merge($options, ['Bucket' => $this->bucket, 'Key' => $path]));
+
+            return $this->service->presign($input, $date);
+        }
+
+        return $this->service->getUrl($this->bucket, $path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setMetadata($key, $metadata)
+    {
+        // BC with AmazonS3 adapter
+        if (isset($metadata['contentType'])) {
+            $metadata['ContentType'] = $metadata['contentType'];
+            unset($metadata['contentType']);
+        }
+
+        $this->metadata[$key] = $metadata;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMetadata($key)
+    {
+        return isset($this->metadata[$key]) ? $this->metadata[$key] : [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read($key)
+    {
+        $this->ensureBucketExists();
+        $options = $this->getOptions($key);
+
+        try {
+            // Get remote object
+            $object = $this->service->getObject($options);
+            // If there's no metadata array set up for this object, set it up
+            if (!array_key_exists($key, $this->metadata) || !is_array($this->metadata[$key])) {
+                $this->metadata[$key] = [];
+            }
+            // Make remote ContentType metadata available locally
+            $this->metadata[$key]['ContentType'] = $object->getContentType();
+
+            return $object->getBody()->getContentAsString();
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rename($sourceKey, $targetKey)
+    {
+        $this->ensureBucketExists();
+        $options = $this->getOptions(
+            $targetKey,
+            ['CopySource' => $this->bucket . '/' . $this->computePath($sourceKey)]
+        );
+
+        try {
+            $this->service->copyObject(array_merge($options, $this->getMetadata($targetKey)));
+
+            return $this->delete($sourceKey);
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write($key, $content)
+    {
+        $this->ensureBucketExists();
+        $options = $this->getOptions($key);
+        unset($options['Bucket'], $options['Key']);
+
+        /*
+         * If the ContentType was not already set in the metadata, then we autodetect
+         * it to prevent everything being served up as binary/octet-stream.
+         */
+        if (!isset($options['ContentType']) && $this->detectContentType) {
+            $options['ContentType'] = $this->guessContentType($content);
+        }
+
+        try {
+            $this->service->upload($this->bucket, $this->computePath($key), $content, $options);
+
+            if (is_resource($content)) {
+                return Util\Size::fromResource($content);
+            }
+
+            return Util\Size::fromContent($content);
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function exists($key)
+    {
+        return $this->service->has($this->bucket, $this->computePath($key));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function mtime($key)
+    {
+        try {
+            $result = $this->service->headObject($this->getOptions($key));
+
+            return $result->getLastModified()->getTimestamp();
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function size($key)
+    {
+        try {
+            $result = $this->service->headObject($this->getOptions($key));
+
+            return $result->getContentLength();
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    public function mimeType($key)
+    {
+        try {
+            $result = $this->service->headObject($this->getOptions($key));
+
+            return $result->getContentType();
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function keys()
+    {
+        return $this->listKeys();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function listKeys($prefix = '')
+    {
+        $this->ensureBucketExists();
+
+        $options = ['Bucket' => $this->bucket];
+        if ((string) $prefix != '') {
+            $options['Prefix'] = $this->computePath($prefix);
+        } elseif (!empty($this->options['directory'])) {
+            $options['Prefix'] = $this->options['directory'];
+        }
+
+        $keys = [];
+        $result = $this->service->listObjectsV2($options);
+        foreach ($result as $file) {
+            $keys[] = $this->computeKey($file['Key']);
+        }
+
+        return $keys;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($key)
+    {
+        try {
+            $this->service->deleteObject($this->getOptions($key));
+
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isDirectory($key)
+    {
+        $result = $this->service->listObjectsV2([
+            'Bucket' => $this->bucket,
+            'Prefix' => rtrim($this->computePath($key), '/') . '/',
+            'MaxKeys' => 1,
+        ]);
+
+        foreach ($result->getContents(true) as $file) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Ensures the specified bucket exists. If the bucket does not exists
+     * and the create option is set to true, it will try to create the
+     * bucket. The bucket is created using the same region as the supplied
+     * client object.
+     *
+     * @throws \RuntimeException if the bucket does not exists or could not be
+     *                           created
+     */
+    protected function ensureBucketExists()
+    {
+        if ($this->bucketExists) {
+            return true;
+        }
+
+        if ($this->bucketExists = $this->service->bucketExists(['Bucket'=>$this->bucket])->isSuccess()) {
+            return true;
+        }
+
+        if (!$this->options['create']) {
+            throw new \RuntimeException(sprintf(
+                'The configured bucket "%s" does not exist.',
+                $this->bucket
+            ));
+        }
+
+        $this->service->createBucket([
+            'Bucket' => $this->bucket
+        ]);
+        $this->bucketExists = true;
+
+        return true;
+    }
+
+    protected function getOptions($key, array $options = [])
+    {
+        $options['ACL'] = $this->options['acl'];
+        $options['Bucket'] = $this->bucket;
+        $options['Key'] = $this->computePath($key);
+
+        /*
+         * Merge global options for adapter, which are set in the constructor, with metadata.
+         * Metadata will override global options.
+         */
+        $options = array_merge($this->options, $options, $this->getMetadata($key));
+
+        return $options;
+    }
+
+    protected function computePath($key)
+    {
+        if (empty($this->options['directory'])) {
+            return $key;
+        }
+
+        return sprintf('%s/%s', $this->options['directory'], $key);
+    }
+
+    /**
+     * Computes the key from the specified path.
+     *
+     * @param string $path
+     *
+     * return string
+     */
+    protected function computeKey($path)
+    {
+        return ltrim(substr($path, strlen($this->options['directory'])), '/');
+    }
+
+    /**
+     * @param string $content
+     *
+     * @return string
+     */
+    private function guessContentType($content)
+    {
+        $fileInfo = new \finfo(FILEINFO_MIME_TYPE);
+
+        if (is_resource($content)) {
+            return $fileInfo->file(stream_get_meta_data($content)['uri']);
+        }
+
+        return $fileInfo->buffer($content);
+    }
+}
